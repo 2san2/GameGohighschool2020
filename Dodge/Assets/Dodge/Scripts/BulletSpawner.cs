84f2998d2540b47d30be45e4a7e0b3d1c5c76cbd
leesan <elite0428@daum.net>
2020-07-02 19:54:18 +0900

    총알피하기 2일차 지갑 푸쉬

---
+++ Dodge/Assets/Dodge/Scripts/BulletSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject m_Bullet;
+    
+    // Update is called once per frame
+    void Update()
+    { 
+        GameObject bullet = GameObject.Instantiate(m_Bullet);
+        bullet.transform.position = transform.position;
+        bullet.transform.rotation = transform.rotation;
+
+        transform.Rotate(0, 1f, 0);
+    }
+}
