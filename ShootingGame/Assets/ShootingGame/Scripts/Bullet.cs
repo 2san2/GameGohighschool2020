edd3563db2364be9727b6e546e602b18d26bc26c
leesan <elite0428@daum.net>
2020-07-20 21:09:30 +0900

    슛팅 1일차

---
+++ ShootingGame/Assets/ShootingGame/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Movement = transform.up * Speed * Time.deltaTime;
+        transform.position += Movement;
+    }
+}
---
+++ ShootingGame/Assets/ShootingGame/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+   
+    void Update()
+    {
+
+
+        //float MoveX = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        //float MoveY = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        
+        
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 1, 0) * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= new Vector3(0, 1, 0) * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= new Vector3(1, 0, 0) * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0) * Speed * Time.deltaTime;
+        }
+        
+        
+    }
+}
