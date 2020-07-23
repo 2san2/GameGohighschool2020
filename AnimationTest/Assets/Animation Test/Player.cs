ff2fe59a85cd89f61bc9ce359dc9833d38c0adc7
leesan <elite0428@daum.net>
2020-07-23 21:25:49 +0900

    애니메이션 연습

---
+++ AnimationTest/Assets/Animation Test/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator san;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            san.SetTrigger("Attack");
+        }
+    }
+}
