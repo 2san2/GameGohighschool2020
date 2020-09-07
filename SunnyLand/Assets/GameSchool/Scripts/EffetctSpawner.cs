6d8b508c5553a6a7acfc02d85ce08806f54b836c
leesan <elite0428@daum.net>
2020-09-07 21:29:06 +0900

    몬스터 + 주인공시점카메라 다시보냄

---
+++ SunnyLand/Assets/GameSchool/Scripts/EffetctSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffetctSpawner : MonoBehaviour
+{
+    public GameObject m_EffectPrefab;
+
+    // Update is called once per frame
+    public void SpawnEffect()
+    {
+        GameObject.Instantiate(m_EffectPrefab, transform.position, transform.rotation);
+    }
+}
